7acfb22ff8976b4f4f611d31c7830845ec998e15
BakryDiarra <bakrydiarra@gmail.com>
2023-03-15 14:05:37 +0000

    Add: SITE_ID = 1 in settings

--- wall_of_shame/settings.py
+++ wall_of_shame/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'contact',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
