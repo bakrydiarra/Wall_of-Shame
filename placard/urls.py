e87d4510c4ae4eaba2acbc8f5a4b66935359c8df
BakryDiarra <bakrydiarra@gmail.com>
2023-03-17 11:08:56 +0000

    Change urls directory. Fix: typing error django tag

--- placard/urls.py
+++ placard/urls.py
@@ -1,8 +1,7 @@
 from . import views
 from django.urls import path
-from .views import PersonaList
 
 
 urlpatterns = [
-    path('persona_list.html/', views.PersonaList.as_view(), name='persona-list')
+    path('', views.PersonaList.as_view(), name='home')
 ]
--- wall_of_shame/urls.py
+++ wall_of_shame/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('persona_list.html/', include('placard.urls'), name='placard_urls'),
+    path('', include('placard.urls'), name='placard_urls'),
 ]
