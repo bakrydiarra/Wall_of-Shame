d3cbe6de929ed849e5c52cebaacf14b3e01e94c6
BakryDiarra <bakrydiarra@gmail.com>
2023-03-17 15:25:21 +0000

    Add: view PersonaDetail

--- placard/views.py
+++ placard/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, view
 from .models import *
 
 
@@ -8,3 +8,23 @@ class PersonaList(generic.ListView):
     queryset = Persona.objects.order_by('created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PersonaDetail(View):
+
+    def get(self, request, *args, **kargs):
+        persona = get_object_or_404(slug=slug)
+        comments = persona.comments.all()
+        Liked = False
+        if persona.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "persona": persona,
+                "comments": comments,
+                "likes": liked
+            },
+        )
