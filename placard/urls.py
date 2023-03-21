ecc86c48835b7e4a2f6754453a8612acc197f4eb
BakryDiarra <bakrydiarra@gmail.com>
2023-03-21 10:38:16 +0000

    Add: url path create_persona. Update: create_persona.html

--- placard/urls.py
+++ placard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PersonaList.as_view(), name='home'),
     path('persona_detail/<slug:slug>/', views.PersonaDetail.as_view(), name='persona_detail'),
     path('like/<slug:slug>', views.PersonaLike.as_view(), name='persona_like'),
+    path('create_persona/', CreatePersonaView.as_view(), name='create_persona'),
 ]
